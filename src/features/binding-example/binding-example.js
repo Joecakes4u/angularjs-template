7c76e6329db6042586850a4bff8e01185607ec53
Joe Wonohadidjojo <joecakes4u@gmail.com>
2018-10-15 16:33:32 -0700

    duplicate files

--- src/features/binding-example/binding-example.js
+++
@@ -1,5 +0,0 @@
-// define Controller "bindingController"
-app.controller('bindingController', function($scope) {
-    $scope.firstName = "Joe";
-    $scope.lastName = "Wonohadidjojo";
-});
--- src/features/login/login.js
+++
@@ -1,8 +0,0 @@
-// define Controller "formController"
-app.controller('formController', function($scope) {
-    $scope.master = {firstName:"John", lastName:"Doe"};
-    $scope.reset = function() {
-        $scope.user = angular.copy($scope.master);
-    };
-    $scope.reset();
-});
